6f1579fa9fa2b3725c07900538d4cc666ea3983f
charlotte <71281295+chaarlottte@users.noreply.github.com>
2023-02-04 13:18:52 -0500

    update version + fix some issues

--- src/main/java/dev/chaarlottte/qprotect/transformer/ASCIIArtTransformer.java
+++ src/main/java/dev/chaarlottte/qprotect/transformer/ASCIIArtTransformer.java
@@ -2,7 +2,6 @@ package dev.chaarlottte.qprotect.transformer;
 
 import dev.mdma.qprotect.api.jar.*;
 import dev.mdma.qprotect.api.transformer.*;
-import dev.mdma.qprotect.api.transformer.exclusions.*;
 import org.objectweb.asm.*;
 import org.objectweb.asm.tree.*;
 import java.util.*;
@@ -47,11 +46,6 @@ public class ASCIIArtTransformer extends ClassTransformer {
         }
         counter.incrementAndGet();
         return true;
-    }
-
-    @Override
-    public ExclusionType getExclusionType() {
-        return ExclusionType.CUSTOM;
     }
 
     private static String getRandomString(int len) {
